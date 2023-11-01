b5bb5b7c0c979165ace0098b2aadd0633b37d32f
Louis Kuang <louiskuang@google.com>
2023-11-01 21:22:23 +0000

    Use advisory lock to avoid clients from being aborted due to concurrent table creation.

--- write_heavy.bench.sql
+++ write_heavy.bench.sql
@@ -1,11 +1,12 @@
-\set txn_value random(0, 100000)
+\set txn_value random(0, 1000000)
 \set write_knob random(0, 100)
-\set table_id random_zipfian(0, 10000, 1.5)
+\set table_id random_zipfian(0, 10000, 1.01)
+-- \set table_id random(0, 10000)
 
 BEGIN;
 
-SELECT create_table_if_not_exist(table_id_to_table_name(:table_id));
+SELECT create_table_if_not_exist(:table_id);
 
-SELECT do_transaction(table_id_to_table_name(:table_id), :write_knob <= 49, :txn_value);
+SELECT do_transaction(:table_id, :write_knob <= 49, :txn_value);
 
 END;
--- write_heavy_utils.sql
+++ write_heavy_utils.sql
@@ -1,25 +1,26 @@
-CREATE OR REPLACE FUNCTION table_id_to_table_name(id int) RETURNS TEXT
-AS $$ SELECT 'test_' || id $$ LANGUAGE SQL;
+CREATE OR REPLACE FUNCTION table_id_to_table_name(table_id int) RETURNS TEXT
+AS $$ SELECT 'test_' || table_id $$ LANGUAGE SQL;
 
-CREATE OR REPLACE FUNCTION create_table_if_not_exist(tab_name text) RETURNS boolean
+CREATE OR REPLACE FUNCTION create_table_if_not_exist(table_id int) RETURNS boolean
 AS $$
 #print_strict_params on
 BEGIN
-    EXECUTE 'CREATE TABLE IF NOT EXISTS ' || tab_name || '(id serial PRIMARY KEY, value int)';
+    PERFORM pg_advisory_xact_lock(table_id);
+    EXECUTE 'CREATE TABLE IF NOT EXISTS ' || table_id_to_table_name(table_id) || '(id serial PRIMARY KEY, value int)';
     return true;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION do_transaction(tab_name text, is_write boolean, value int) RETURNS boolean
+CREATE OR REPLACE FUNCTION do_transaction(table_id int, is_write boolean, value int) RETURNS boolean
 AS $$
 #print_strict_params on
 DECLARE
 ret boolean;
 BEGIN
     IF is_write THEN
-        EXECUTE 'INSERT INTO ' || tab_name || '(value) VALUES (' || value || ')';
+        EXECUTE 'INSERT INTO ' || table_id_to_table_name(table_id) || '(value) VALUES (' || value || ')';
     ELSE
-        EXECUTE 'SELECT * FROM ' || tab_name || ' WHERE id = ' || value;
+        EXECUTE 'SELECT * FROM ' || table_id_to_table_name(table_id) || ' WHERE id = ' || value;
     END IF;
     return true;
 END;
