1e05120c3e88009f8054b87f72945e544bd22c6a
Louis Kuang <louiskuang@google.com>
2023-11-02 03:14:21 +0000

    Add some notes about the use of advisory lock.

--- write_heavy.bench.sql
+++ write_heavy.bench.sql
@@ -1,7 +1,6 @@
 \set txn_value random(0, 1000000)
 \set write_knob random(0, 100)
 \set table_id random_zipfian(0, 10000, 1.01)
--- \set table_id random(0, 10000)
 
 BEGIN;
 
--- write_heavy_utils.sql
+++ write_heavy_utils.sql
@@ -5,6 +5,14 @@ CREATE OR REPLACE FUNCTION create_table_if_not_exist(table_id int) RETURNS boole
 AS $$
 #print_strict_params on
 BEGIN
+    -- This lock is only necessary in the benchmark to avoid clients from being aborted due
+    -- to concurrent `CREATE TABLE` DDL. In a real workload, we should be able to ignore error
+    -- due to race to create etable. An alternative is to use the single check idiom to first
+    -- check if the table exists and only create the table if the table does not exist. However,
+    -- the query itself ends up dominating the latency of the overall workload due to complicated
+    -- join required when querying `information_schema.tables`
+    -- As long as the `table_id` is drawn from a large enough range, the chances of two clients waiting
+    -- accessing the same table and thus landing on the same lock should be low.
     PERFORM pg_advisory_xact_lock(table_id);
     EXECUTE 'CREATE TABLE IF NOT EXISTS ' || table_id_to_table_name(table_id) || '(id serial PRIMARY KEY, value int)';
     return true;
